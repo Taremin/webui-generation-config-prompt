cba0c885cf0ed5026b829c23f8b0f4efcf469b68
Taremin <taremin.vr@gmail.com>
2023-12-12 00:45:49 +0900

    webui1.6.0以降はbefore_process_batchの前に解像度が決定されるため, width/heightの変更が無効になっていたため処理を修正 rngの有無で処理を分岐しているのは以前のバージョンでも動作するようにするため

--- scripts/generation-config-prompt.py
+++ scripts/generation-config-prompt.py
@@ -1,5 +1,6 @@
 import modules.scripts as scripts
 import modules.extra_networks as extra_networks
+import modules.processing as processing
 import gradio as gr
 
 class GenarationConfigPrompt(scripts.Script):  
@@ -12,12 +13,12 @@ class GenarationConfigPrompt(scripts.Script):
     def ui(self, is_img2img):
         with gr.Accordion(self.title(), open=False):
             with gr.Row():
-                with gr.Column(min_width = 50, scale=1):
+                with gr.Column(min_width=50, scale=1):
                     trigger = gr.Textbox(
                         label="Triger Word",
                         lines=1,
                         value="config",
-                        interactive =True,
+                        interactive=True,
                         elem_id="genconfprompt_trigger_word"
                     )
                     
@@ -37,12 +38,34 @@ class GenarationConfigPrompt(scripts.Script):
             class ConfigProcessor:
                 def swap():
                     p.width, p.height = p.height, p.width
+                    if hasattr(p, "rng"):
+                        shape = list(p.rng.shape)
+                        shape[1] = p.height // processing.opt_f
+                        shape[2] = p.width // processing.opt_f
+                        p.rng.shape = tuple(shape)
+
                 def set():
                     for key, val in config.named.items():
                         if not hasattr(p, key):
                             continue
-                        setattr(p, key, type(getattr(p, key))(val))
-            
+                        
+                        value_type = type(getattr(p, key))
+                        if key == "cfg_scale":
+                            value_type = float
+                            
+                        setattr(p, key, value_type(val))
+                        
+                        if key == "width":
+                            if hasattr(p, "rng"):
+                                shape = list(p.rng.shape)
+                                shape[2] = p.width // processing.opt_f
+                                p.rng.shape = tuple(shape)
+                        elif key == "height":
+                            if hasattr(p, "rng"):
+                                shape = list(p.rng.shape)
+                                shape[1] = p.height // processing.opt_f
+                                p.rng.shape = tuple(shape)
+                        
             funcname = config.positional[0]
             if not hasattr(ConfigProcessor, funcname):
                 continue
